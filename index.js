5f09ec8bc2a34a6d6573b1df5d25cc76a497b79e
Raul <email@email.com>
2021-06-14 11:27:03 +0300

    final new change

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 
-app.use(express.static(__dirname + 'client'));
+if(process.env.NODE_ENV === 'production'){
 
-app.get('*',(req,res)=>{
-    res.sendFile('client/index.html', {root: __dirname })
-});
+    app.use(express.static(__dirname + 'client'));
+
+    const path = require('path');
+    app.get('*',(req,res)=>{
+        res.sendFile('client/index.html', {root: __dirname })
+    });
+
+};
 
 const PORT = process.env.PORT || 5000;
 
